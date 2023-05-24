b4e3afc001f8f316a47a3d56444546d7b81a342a
Tomas-Simoes <tomas20simoes@gmail.com>
2023-05-24 13:03:41 +0100

    Calls upated

--- back-end/app.js
+++ back-end/app.js
@@ -3,18 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 const mongoURI =
-  "mongodb+srv://simoes:simoes12345@cluster0.qksi9tv.mongodb.net/?retryWrites=true&w=majoritymongodb+srv://simoes:<password>@cluster0.qksi9tv.mongodb.net/?retryWrites=true&w=majority";
-
-async function connectMongo() {
-  try {
-    await mongoose.connect(mongoURI);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-connectMongo();
+  "mongodb+srv://simoes:simoes12345@cluster0.qksi9tv.mongodb.net/noteApp?retryWrites=true&w=majority";
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -27,8 +16,51 @@ app.use((req, res, next) => {
   next();
 });
 
+//? MONGO
+async function connectMongo() {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+connectMongo();
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+//? ROUTES
 app.get("/", async (req, res) => {
-  return res.json({ response: "This is the response from port 8080" });
+  return res.json({ response: "This is the response from backend" });
+});
+
+app.get("/add", async (req, res) => {
+  var noteCount = await Note.find({}).then((result) => {
+    return result.length;
+  });
+
+  const newNote = new Note({
+    content: "This is note number " + (noteCount + 1),
+    important: true,
+  });
+
+  newNote.save().then((result) => {
+    return res.json({ response: "Note saved successfully!" });
+  });
+});
+
+app.get("/dbcount", async (req, res) => {
+  connectMongo();
+
+  Note.find({}).then((result) => {
+    return res.json({ count: result.length });
+  });
 });
 
 /*
--- next-app/src/pages/index.js
+++ next-app/src/pages/index.js
@@ -7,6 +7,12 @@ export default function Home() {
       );
 
       const res = await fetch(`https://backendtest.famousgadget.pt/`);
+
+      /*
+      console.log("Requesting API over: 'http://localhost:8080'");
+      const res = await fetch("http://localhost:8080");
+      */
+
       const data = await res.json();
 
       console.log(data);
@@ -14,10 +20,54 @@ export default function Home() {
       console.log(err);
     }
   };
+
+  const addToDatabase = async () => {
+    try {
+      console.log(
+        "Requesting API over: 'https://backendtest.famousgadget.pt/add'"
+      );
+
+      const res = await fetch(`https://backendtest.famousgadget.pt/add`);
+
+      /*
+      console.log("Requesting API over: 'http://localhost:8080/add'");
+      const res = await fetch("http://localhost:8080/add");
+      */
+
+      const data = await res.json();
+
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const countDatabase = async () => {
+    try {
+      console.log(
+        "Requesting API over: 'https://backendtest.famousgadget.pt/dbcount'"
+      );
+
+      const res = await fetch(`https://backendtest.famousgadget.pt/dbcount`);
+
+      /*
+        console.log("Requesting API over: 'http://localhost:8080/dbcount'");
+        const res = await fetch("http://localhost:8080/dbcount");
+      */
+
+      const data = await res.json();
+
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <button onClick={callAPI}>Make API call</button>
+        <button onClick={addToDatabase}>Add item to database</button>
+        <button onClick={countDatabase}>Get item count</button>
       </main>
     </div>
   );
